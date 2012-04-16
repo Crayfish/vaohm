885b277b2a6a73cfb1cd00f20f43df2ad9f898b2
mark.ormos <mark.ormos@gmail.com>
2012-04-16 09:49:05 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String osName = System.getProperty("os.name");
+		try {
+			if (osName.contains("Windows")) {
+				UIManager
+						.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			} else
+				UIManager.setLookAndFeel(UIManager
+						.getCrossPlatformLookAndFeelClassName());
+
+		} catch (UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException e) {
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		}
+
+		new GUI();
+
+	}
+
+}
